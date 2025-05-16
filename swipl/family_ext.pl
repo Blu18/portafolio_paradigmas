6f9ed988558f465d748cf5bbd299e8486f50db8a
Blu18 <diego.manzo@uabc.edu.mx>
2025-05-16 10:30:16 -0700

    Se añadieron bases de conocimiento

---
+++ swipl/family_ext.pl
@@ -0,0 +1,30 @@
+female(pam).
+female(liz).
+female(pat).
+female(ann).
+
+male(jim).
+male(bob).
+male(tom).
+male(peter).
+
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+parent(bob, peter).
+parent(peter, jim).
+
+mother(X,Y) :- parent(X,Y), female(X).
+father(X,Y) :- parent(X,Y), male(X).
+haschild(X) :- parent(X,_).
+sister(X,Y) :- parent(Z,X), parent(Z,Y), X\==Y.
+brother(X,Y) :- parent(Z,X), parent(Z,Y), X\==Y.
+
+grandparent(X,Y) :- parent(X,Z), parent(Z,Y).
+grandparent(X,z) :- mother(X,Y), parent(Y,Z).
+grandfather(X,Z) :- father(X,Y), parent(Y,Z).
+wife(X,Y) :- parent(X,Z), parent(Y,Z), female(X), male(Y).
+uncle(X,Z) :- brother(X,Y), parent(Y,Z).
